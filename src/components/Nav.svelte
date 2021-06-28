75db1d89ccbe8e8f58c75ca58b6d4bcb6119945c
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-06-28 16:21:44 -0500

    Se realizan cambios en el template para traer los assets

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,85 +1,106 @@
 <script>
-	export let segment;
+  const collapseMenu = () => {
+    if (window.innerWidth < 992) {
+      document
+        .querySelector("#navbarTogglerDemo01")
+        .classList.replace("collapse", "collapsing");
+      document.querySelector("#navbarTogglerDemo01").classList.remove("show");
+      document.querySelector("#navbarTogglerDemo01").classList.add("collapse");
+    }
+  };
 </script>
 
-<style>
-	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
+<div class="main_menu_bg ">
+  <div class="row main-menu">
+    <nav class="navbar navbar-expand-lg navbar-dark ">
+      <div class="row">
+        <div class="col-sm-12 col-md-2">
+          <img src="./img/Logo foodzone.png" alt="" class="imagen-logo" />
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon" />
+          </button>
+        </div>
 
-	ul {
-		margin: 0;
-		padding: 0;
-	}
-
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
-
-	li {
-		display: block;
-		float: left;
-	}
-
-	[aria-current] {
-		position: relative;
-		display: inline-block;
-	}
-
-	[aria-current]::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	}
-
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	}
-</style>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-    </div>
+        <div class="col-sm-12 col-md-10">
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <ul class="navbar-nav px-0 ">
+              <li class="nav-item" on:click={collapseMenu}>
+                <a href="/" class="nav-link active activo">Home</a>
+              </li>
+              <li class="nav-item" on:click={collapseMenu}>
+                <a href="colaboradores" class="nav-link active activo"
+                  >Colaboradores</a
+                >
+              </li>
+              <li class="nav-item" on:click={collapseMenu}>
+                <a href="nosotros" class="nav-link active activo">Nosotros</a>
+              </li>
+              <li class="nav-item" on:click={collapseMenu}>
+                <a href="pedir" class="nav-link active activo">Pedido</a>
+              </li>
+              <li class="nav-item" on:click={collapseMenu}>
+                <a href="noticias" class="nav-link active activo">Noticias</a>
+              </li>
+              <li class="nav-item" on:click={collapseMenu}>
+                <a href="preguntas-frecuentes" class="nav-link active activo"
+                  >Preguntas
+                </a>
+              </li>
+              <li class="nav-item" on:click={collapseMenu}>
+                <a href="terminos-condiciones" class="nav-link active activo"
+                  >Términos</a
+                >
+              </li>
+              <li class="nav-item" on:click={collapseMenu}>
+                <a href="contacto" class="nav-link active activo">Contacto</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
   </div>
-</nav>
-<nav>
-	<ul>
-		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">Casa</a></li>
-		<li><a aria-current="{segment === 'colaboradores' ? 'page' : undefined}" href="colaboradores">Colaboradores</a></li>
-		<li><a rel=prefetch aria-current="{segment === 'nosotros' ? 'page' : undefined}" href="nosotros">Nosotros</a></li>
-		<li><a rel=prefetch aria-current="{segment === 'noticias' ? 'page' : undefined}" href="noticias">Noticias</a></li>
-		<li><a rel=prefetch aria-current="{segment === 'pedido' ? 'page' : undefined}" href="pedido">Pedido</a></li>
-		<li><a rel=prefetch aria-current="{segment === 'preguntas' ? 'page' : undefined}" href="preguntas">Preguntas</a></li>
-		<li><a rel=prefetch aria-current="{segment === 'terminos' ? 'page' : undefined}" href="terminos">Términos</a></li>
-		<li><a rel=prefetch aria-current="{segment === 'contacto' ? 'page' : undefined}" href="contacto">Contacto</a></li>
-	</ul>
-</nav>
+</div>
+
+<style>
+  .main-menu {
+    margin-left: 10%;
+    margin-right: 10%;
+  }
+  .imagen-logo {
+    width: 30%;
+    height: auto;
+    z-index: 100000000111111;
+  }
+  .navbar-nav {
+    margin-left: 30%;
+  }
+  .navbar-nav:hover {
+    color: black;
+  }
+  .navbar-toggler {
+    margin-left: 20%;
+    color: white;
+  }
+  .main_menu_bg {
+    background-color: rgba(black, black, black, 0.5);
+  }
+  .navbar-toggler-icon {
+    color: white;
+    z-index: 11111111111110000000000000;
+  }
+  @media only screen and (min-width: 320px) and (max-width: 991px) {
+    .imagen-logo {
+      width: 10%;
+      margin-top: 0%;
+    }
+  }
+</style>
