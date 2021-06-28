2a211a923ee0748bb745969119922fdb570affe9
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-06-28 16:07:31 -0500

    vista pedir

---
+++ src/routes/pedir/index.svelte
@@ -0,0 +1,103 @@
+<script>
+</script>
+
+<div class="row ">
+  <main>
+    <div class="position-relative overflow-hidden  bg-light pb-5">
+      <div class="col-md-12">
+        <div class="row justify-content-center">
+          <div class="col-8 py-5">
+            <h4
+              class="text-center wow animated rubberBand "
+              data-animation="flash"
+            >
+              ¿Cómo realizar un pedido?
+            </h4>
+
+            <div
+              class="row row-cols-1 row-cols-md-3 g-4 justify-content-center"
+            >
+              <div class="col">
+                <div class="card">
+                  <img
+                    src="./img/FZ2.png"
+                    class="mx-auto"
+                    alt="..."
+                    style="width: 40%;"
+                  />
+                  <div class="card-body">
+                    <h5 class="card-title text-center">
+                      Regístrate en Food Zone
+                    </h5>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col">
+                <div class="card">
+                  <img
+                    src="./img/FZ4.png"
+                    class="card-img-top center-img"
+                    alt="..."
+                  />
+                  <div class="card-body">
+                    <h5 class="card-title text-center">
+                      Escaneé el código QR de tu mesa
+                    </h5>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col">
+                <div class="card ">
+                  <img
+                    src="./img/FZ3.png"
+                    class="card-img-top mx-auto"
+                    alt="..."
+                    style="width: 40%;"
+                  />
+                  <div class="card-body">
+                    <h5 class="card-title text-center">Realiza tu pedido</h5>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col">
+                <div class="card">
+                  <img
+                    src="./img/FZ6.png"
+                    class="card-img-top mx-auto"
+                    alt="..."
+                    style="width: 40%;"
+                  />
+                  <div class="card-body text-center">
+                    <h5 class="card-title text-center">Evalua servicio</h5>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col">
+                <div class="card">
+                  <img
+                    src="./img/FZ5.png"
+                    class="card-img-top mx-auto"
+                    alt="..."
+                    style="width: 40%;"
+                  />
+                  <div class="card-body ">
+                    <h5 class="card-title text-center">
+                      Recibe una recompensa
+                    </h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</div>
+
+<style>
+</style>
